7298756a8d42dcb21520753e8aec8f9afd92d008
Antdao <94835663+Antdao@users.noreply.github.com>
2022-05-09 16:14:20 +0800

    Update FirstTest.sol

--- contracts/test/FirstTest.sol
+++ contracts/test/FirstTest.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract FirstTest {
+    mapping (address => uint256)  _balances;
 
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    
 }
