fa61091060824c911ac8784bce8180dd214de493
Antdao <94835663+Antdao@users.noreply.github.com>
2022-05-17 16:16:30 +0800

    Create RefundEscrow.sol

---
+++ contracts/test/utils/escrow/RefundEscrow.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ConditionalEscrow.sol";
+
+
+contract RefundEscrow is ConditionalEscrow {
+    using Address for address payable;
+
+    enum State {
+        Active,
+        Refunding,
+        Closed
+    }
+
+    event RefundsClosed();
+    event RefundsEnabled();
+
+    State private _state;
+    address payable private immutable _beneficiary;
+
+    /**
+     * @dev Constructor.
+     * @param beneficiary_ The beneficiary of the deposits.
+     */
+    constructor(address payable beneficiary_) {
+        require(beneficiary_ != address(0), "RefundEscrow: beneficiary is the zero address");
+        _beneficiary = beneficiary_;
+        _state = State.Active;
+    }
+
+   
+}
